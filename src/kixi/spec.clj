8d8f4488c63d483d9b3048979f62c84a4399ecc2
Antony Woods <antony@mastodonc.com>
2017-09-21 15:28:25 +0100

    Initial commit

---
+++ src/kixi/spec.clj
@@ -0,0 +1,6 @@
+(ns kixi.spec)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/kixi/spec_test.clj
@@ -0,0 +1,7 @@
+(ns kixi.spec-test
+  (:require [clojure.test :refer :all]
+            [kixi.spec :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
