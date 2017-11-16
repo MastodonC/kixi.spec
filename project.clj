7d3bfb3399156ea814eff7ce2c46d378c02a548a
Antony Woods <antony@mastodonc.com>
2017-11-16 14:15:08 +0000

    Version 0.1.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/kixi.spec "0.1.12-SNAPSHOT"
+(defproject kixi/kixi.spec "0.1.12"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
